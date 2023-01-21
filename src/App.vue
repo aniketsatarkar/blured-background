c4c80894dbd61329c70ada5ebc7b83b10245dbe8
Aniket <aniketsatarkar9@gmail.com>
2023-01-21 22:39:10 +0530

    implemented a gradient plugin

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div :style="gradient" class="width: 250px; height: 350px">
+      <img :src="image" class="inline" />
+    </div>
+
+    <div class="inline" :style="gradient"></div>
+    <br />
+    <div
+      class="multi-repeating-linear"
+      style="width: 200px; height: 300px"
+    ></div>
+    <br />
+    <textarea v-model="gradient"></textarea>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+var grad = require("gradient-from-image");
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      gradient: null,
+      image: "https://picsum.photos/200/300?image=6",
+    };
+  },
+  created() {
+    this.getGradient();
+  },
+  methods: {
+    async getGradient() {
+      const imgUrl = this.image;
+      let gradient = null;
+
+      await grad.gr(imgUrl).then((response) => {
+        gradient = response;
+      });
+
+      console.log(gradient);
+
+      let colors = gradient.relevant;
+
+      this.gradient = `background: linear-gradient(45deg, ${colors[1]}, ${colors[0]});`;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.inline {
+  display: inline-block !important;
+  width: 200px !important;
+  height: 300px !important;
+}
+
+.multi-repeating-linear {
+  background: repeating-linear-gradient(190deg, #642b73, #c6426e),
+    repeating-linear-gradient(-190deg, #642b73, #c6426e),
+    repeating-linear-gradient(23deg, #642b73, #c6426e);
 }
 </style>
